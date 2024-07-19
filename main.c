505b2e9e6a4acbd86bc294cad8a431efc2c536f8
jc <46619361+juancwu@users.noreply.github.com>
2024-07-19 01:59:31 -0400

    init template

--- main.c
+++ main.c
@@ -1,138 +1,89 @@
-#include <stdio.h>
-#include <stdlib.h>
+#include <asm-generic/ioctls.h>
+// #include <stdio.h>
+#include <sys/ioctl.h>
 #include <unistd.h>
 
-// default width of 22 squares in width
-#define WIDTH 22
-// default of 33 squares in height
-#define HEIGHT 23
-// the extra spaces in the width
-#define WIDTH_OFFSET 1
-// the extra spaces in the height
-#define HEIGHT_OFFSET 1
+#define HEIGHT 16
+#define WIDTH 10
+#define TRUE 1
+#define FALSE 0
+#define CLEAR_SCREEN_AND_HIDE_CURSOR "\033[2J\033[?25l"
+#define SHOW_CURSOR "\033[?25h"
 
-enum TetrominoShape { I, O, T, S, Z, J, L };
-enum Orientation { Top, Right, Bottom, Left };
+// A virtual grid to represent the state of the playfield.
+// This makes it easier to do collision detection, rotation and movement.
+// Then when everything has been checked, the virtual grid is rendered.
+int virtual_grid[HEIGHT][WIDTH];
 
-// small to big represents the points gotten from 1, 2, 3, or 4+ lines.
-const unsigned short int POINTS[4] = {100, 300, 500, 800};
+// A rendered grid contains the visual representation of the virtual grid.
+// It has 2 more height and width than the virtual grid because it renders
+// borders as well. However, the borders are not included into the virtual grid
+// because we do not need it for evaluation.
+//
+// width * 2 is to compensate the size of a block drawn with [].
+char rendered_grid[HEIGHT + 2][WIDTH * 2 + 2];
 
-// map/grid of tetris
-char grid[HEIGHT][WIDTH];
-// the current tetromino being manipulated
-enum TetrominoShape current_tetromino;
-// the current orientation of the block being manipulated
-enum Orientation current_orientation;
-// the current position of the current tetromino, many currents
-int current_x, current_y;
+// This represents the different tetromino available.
+enum Tetromino {
+    I,
+    O,
+    T,
+    S,
+    Z,
+    J,
+    L,
+};
 
-void empty_grid() {
+// This represents the an orientation for a shape. Makes it easier to calculate
+// movements and rotations to render.
+enum Orientation {
+    Top,
+    Right,
+    Bottom,
+    Left,
+};
+
+// Current shape properties
+enum Tetromino current_shape;
+enum Orientation current_shape_orientation;
+int current_y, current_x;
+
+// Initialize the game, includes playfield and picks the starting tetromino.
+int init() {
     for (int y = 0; y < HEIGHT; y++) {
         for (int x = 0; x < WIDTH; x++) {
-            if (y == 0 || y == HEIGHT - 2) {
-                grid[y][x] = '-';
-            } else if (x == 0 || x == WIDTH - 1) {
-                grid[y][x] = ':';
+            virtual_grid[y][x] = 0;
+        }
+    }
+    for (int y = 0; y < HEIGHT + 2; y++) {
+        for (int x = 0; x < WIDTH + 2; x++) {
+            if (y == 0 || y == HEIGHT) {
+                rendered_grid[y][x] = '-';
+            } else if (x == 0 || x == WIDTH) {
+                rendered_grid[y][x] = ':';
             } else {
-                grid[y][x] = ' ';
+                rendered_grid[y][x] = ' ';
             }
         }
     }
+    return 0;
 }
 
-void paint_block() {
-    switch (current_tetromino) {
-    case I:
-        // initial shape
-        // ####
-        switch (current_orientation) {
-        case Top:
-        case Bottom:
-            for (int i = 0; i < 4; i++) {
-                grid[current_y + HEIGHT_OFFSET][current_x + i] = '[';
-                grid[current_y + HEIGHT_OFFSET][current_x + i + 1] = ']';
-            }
-            break;
-        case Right:
-        case Left:
-            for (int i = 0; i < 4; i++) {
-                grid[current_y + i + HEIGHT_OFFSET][current_x] = '[';
-                grid[current_y + i + HEIGHT_OFFSET][current_x + 1] = ']';
-            }
-            break;
-        }
-        break;
-    case O:
-        // initial shape
-        // ##
-        // ##
-        for (int y = 0; y < 2; y++) {
-            for (int x = 0; x < 4; x += 2) {
-                grid[current_y + y + HEIGHT_OFFSET]
-                    [current_x + x + WIDTH_OFFSET] = '[';
-                grid[current_y + y + HEIGHT_OFFSET]
-                    [current_x + x + WIDTH_OFFSET + 1] = ']';
-            }
-        }
-        break;
-    case T:
-    case S:
-    case Z:
-    case J:
-    case L:
-        break;
-    }
-}
+// Update the virtual grid according to various states.
+int update() { return 0; }
 
-void print_grid() {
-    // Move the cursor to the top-left corner of the terminal
-    printf("\033[H");
-    for (int y = 0; y < HEIGHT; y++) {
-        for (int x = 0; x < WIDTH; x++) {
-            printf("%c", grid[y][x]);
-        }
-        printf("\n");
-    }
-}
-
-// this should be call after update
-void view() {
-    // reset the grid
-    empty_grid();
-    // paint the current tetromino
-    paint_block();
-    // print it to console
-    print_grid();
-}
-
-void spawn_block() {
-    // int index = rand() % 7;
-    int index = O;
-    current_tetromino = index;
-    current_x = WIDTH / 2 - 2;
-    current_y = 0;
-}
-
-void update() { current_y = (current_y + 1) % (HEIGHT - HEIGHT_OFFSET); }
+// Renders the virtual grid.
+int view() { return 0; }
 
 int main() {
-    // 500ms
-    int print_rate = 500 * 1000;
+    // struct winsize w;
+    // if (ioctl(STDOUT_FILENO, TIOCGWINSZ, &w) == -1) {
+    //     perror("ioctl");
+    //     return 1;
+    // }
 
-    printf("It's Tetris, ON THE TERMINAL :o\n");
-
-    empty_grid();
-    spawn_block();
-    current_orientation = Left;
-    // Clear the screen and hide the cursor
-    printf("\033[2J\033[?25l");
-    while (1) {
-        update();
-        view();
-        usleep(print_rate);
-    }
-    // Show the cursor again before exiting
-    printf("\033[?25h");
+    // printf("Rows: %d\n", w.ws_row);
+    // printf("Cols: %d\n", w.ws_col);
 
     return 0;
 }
